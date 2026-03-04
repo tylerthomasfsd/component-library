ee3cf0829981c4df17b5f7bcca388c75493dbfb5
tylerthomas <thomascrank96@gmail.com>
2026-03-04 15:56:23 -0600

    Made update to sliding button to prevent the overlay from drifting to the side when resizing the window

--- src/components/SlidingButton.tsx
+++ src/components/SlidingButton.tsx
@@ -1,6 +1,7 @@
+import { Paper } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 
-const TabButtons = ({
+const SlidingButton = ({
   defaultButton,
   buttons,
   overlayColorClasses,
@@ -13,6 +14,8 @@ const TabButtons = ({
 }) => {
   /* USESTATE VARIABLES */
   const [selectedTab, setSelectedTab] = useState<string>(defaultButton);
+  const [selectedButton, setSelectedButton] =
+    useState<HTMLButtonElement | null>(null);
   const [windowSize, setWindowSize] = useState<number>(0);
 
   /* REFS */
@@ -24,12 +27,14 @@ const TabButtons = ({
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     event.stopPropagation();
+
     if (!buttonOverlayRef.current) {
       console.log(buttonOverlayRef.current);
       return;
     }
 
     setSelectedTab(buttonLabel);
+    setSelectedButton(event.currentTarget);
 
     const targetLocation = event.currentTarget.offsetLeft;
 
@@ -40,46 +45,57 @@ const TabButtons = ({
   useEffect(function trackWindowResize() {
     const handleResize = () => {
       setWindowSize(window.innerWidth);
-    }
+    };
     window.addEventListener("resize", handleResize);
-
-    return () => {
-        window.removeEventListener("resize", handleResize);
-    }
-  })
+  });
 
   useEffect(
-    function setOverlayWidth() {
+    function handleWindowResize() {
       if (!buttonOverlayRef.current) {
         return;
       }
 
+      let targetLocation = 0;
+
+      if (selectedButton) {
+        targetLocation = selectedButton.offsetLeft;
+      }
+
+      buttonOverlayRef.current.classList.remove("transition");
+      buttonOverlayRef.current.style.translate = `${targetLocation}px`;
       buttonOverlayRef.current.style.width = `${100 / buttons.length}%`;
+
+      setTimeout(function addTransition() {
+        buttonOverlayRef.current?.classList.add("transition");
+      }, 10);
     },
-    [buttons.length, windowSize],
+    [buttons.length, selectedButton, windowSize],
   );
 
   return (
-    <div className="flex relative w-full p-0.5 bg-white rounded-full z-20">
+    <Paper
+      className="flex relative w-full p-0.5 bg-white rounded-full! z-20"
+      elevation={4}
+    >
       <div
         className={`flex items-center flex-1 relative w-full rounded-full ${fontFamilyClasses}`}
       >
         {buttons.map((button, index) => (
           <button
             key={index}
-            className={`flex-1 py-1 px-3 rounded-full text-nowrap text-[0.75rem] sm:text-sm md:text-md lg:text-lg lg:text-[1.175rem] ${selectedTab === button ? "text-white" : "text-black"} cursor-pointer z-10`}
+            className={`flex-1 py-1 px-3 rounded-full text-nowrap text-[0.75rem] sm:text-sm md:text-md lg:text-lg lg:text-[1.175rem] ${selectedTab === button ? "text-white text-shadow-2xs text-shadow-black" : "text-black"} cursor-pointer z-10`}
             onClick={(e) => handleButtonClick(button, e)}
           >
             {button}
           </button>
         ))}
         <div
-          className={`${overlayColorClasses} absolute inset-0 transition rounded-full`}
+          className={`${overlayColorClasses} absolute inset-0 rounded-full transition`}
           ref={buttonOverlayRef}
         ></div>
       </div>
-    </div>
+    </Paper>
   );
 };
 
-export default TabButtons;
+export default SlidingButton;
